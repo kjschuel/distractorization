001cd54c9e636fb59191f1a9cda3d22e1c6ce3c4
Kyle <kjschuel94@gmail.com>
2024-06-09 04:31:32 -0400

    shying away from a tree/recursion approach to a loop/array approach

--- main.py
+++ main.py
@@ -1,33 +1,10 @@
 import sys
 import os
+import src.tree as tree
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 DIGITS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-class Tree:
-    def __init__(self, value):
-        self.value = value
-        self.children = []
-
-    def add_child(self, child_node):
-        self.children.append(child_node)
-
-    def __repr__(self):
-        return f"TreeNode(value={self.value}, children={self.children})"
-
-def step1(z):
-    z1 = z % 10
-    possibles = {}
-    for x1 in DIGITS:
-        for y1 in DIGITS:
-            if ((x1 * y1) % 10 == z1):
-                pair = sorted([x1, y1])
-                new_possible = str(pair[0]) + ',' + str(pair[1])
-                if (possibles.get(new_possible) == None):
-                    possibles[new_possible] = ''
-    possibles['digits'] = 1        
-    return possibles
     
 def step2(z, x1, y1):
     knows = [x1, y1]
@@ -66,16 +43,11 @@ if __name__ == '__main__':
     
     #step 5. profit
     
+    N = 9
+    potentials = tree.attempt_build(N, max_iter=2)
+    result = tree.check(N, potentials)
+    print(result)
     
-    
-    
-    
-    
-    
-    
-    
-    print(step1(5063))
-    print(step2(5063, 1, 3)) 
     
     
     
--- src/tree.py
+++ src/tree.py
@@ -1,11 +1,113 @@
+import re
+
+
 # we are going to need a tree structure.
+# but might go with arrays first when getting started,.
+# should be fine with small numbers and is easier for me to understand 
+# as I get off the ground
 
 # I don't think an onject is a good idea because it will be big
 
 # And when I say big... I mean BIG. Like take all your RAM big.
 
 # So maybe we save it to a file. Maybe we make it a json.
+# but if it's take all your RAM big... lets not be saving files that big...
 
 # We will also need to traverse this tree... so we will want some sort 
 # traversal, checkpoint, resumable aspect.
 
+
+def build(N):
+    
+    print(step1(N))
+    
+    
+    return None
+
+
+def traverse():
+    # need some sort of checkpoint and resume here
+    return None
+
+
+
+def write():
+    # thinking a json with some info, including checkpoint info at the top level
+    return None
+
+def step1(n1):
+    potentials = []
+    for p1 in range(10):
+        for q1 in range(p1, 10): # because at this point we dont care which we pick to be p or q
+            if ((p1 * q1) % 10 == n1):
+                potentials.append([p1, q1])
+    return potentials
+
+
+def step1_tree_attempt(n1):
+    possibles = {}
+    for p1 in range(10):
+        for q1 in range(10):
+            if ((p1 * q1) % 10 == n1):
+                # since this is the last digit, what we choose to be P or Q does not matter
+                # in fact, we don't need BOTH combinations
+                pair = sorted([p1, q1])
+                new_possible = str(pair[0]) + ',' + str(pair[1])
+                if (possibles.get(new_possible) == None):
+                    possibles[new_possible] = {}
+    possibles['digits'] = 1
+    return possibles
+
+def adjust():
+    return None
+
+
+def terminate(max_iter, iter):
+    # TODO: maybe detemrined by len(P)+len(Q) and how it compares to len(N)
+    return max_iter == iter
+
+def check(N, potentials):
+    for potential in potentials:
+        P = potential[0]
+        Q = potential[1]
+        # TODO: this will need to be a string multiplication eventually
+        if (P*Q == N):
+            return [P, Q]
+    print('failure')
+    return False
+        
+
+def attempt_build(N, max_iter = 1, ps=[], qs=[]):
+    # TODO: error checking of ps and qs. 
+    #   should begin empty.
+    #   should be same length
+    #   each entry should be the same number of digits
+
+    potentials = step1(N % 10)
+
+    iter = 1
+    
+    while not terminate(max_iter, iter):
+        for potential in potentials:
+            P = potential[0]
+            Q = potential[1]
+            N_prime = int((N - P*Q) / (10**iter))
+            # TODO: need to build the next digit finder
+            # p1q2 + p2q1 mod 10  = N_prime 
+            print(N_prime)
+        # keep building
+        iter += 1
+    
+    
+    
+    return potentials
+
+# def step2(n2_prime, p1, q1):
+#     possibles = {}
+#     n2_prime = int(((z - (x1 * y1)) / 10 )) % 10
+#     for x2 in DIGITS:
+#         for y2 in DIGITS:
+#             if (((x2 * y1) + (x1 * y2)) % 10 == int(z2_prime)):       
+#                 new_possible = str(x2) + ',' + str(y2)         
+#                 possibles.append(new_possible)
+#     return possibles    
